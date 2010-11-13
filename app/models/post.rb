e845f1c8495ecdb0952e20325b408d01839fbc54
aRailsDemo <arailsdemo@gmail.com>
2010-11-13 12:32:53 -0800

    Modelling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  default_scope order("sequence DESC")
+  
+  validates :title, :presence => true
+  validates :sequence, :presence => true,
+                       :numericality => { :greater_than => 0 }
+  validates :description, :presence => true
 end
