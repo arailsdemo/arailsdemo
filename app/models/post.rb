f1665bddc1d2a416035fefbdde978af26afe6c6a
aRailsDemo <arailsdemo@gmail.com>
2010-11-21 20:18:50 -0800

    Updating the Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
   default_scope order("sequence DESC")
   
+  has_many :sections, :dependent => :destroy
+  accepts_nested_attributes_for :sections, :reject_if => :all_blank
+  
+  
   validates :title, :presence => true
   validates :sequence, :presence => true,
                        :numericality => { :greater_than => 0 }
