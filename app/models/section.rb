2cd994bd07c06c9102da9f3545b63129317a153f
aRailsDemo <arailsdemo@gmail.com>
2010-11-21 20:17:32 -0800

    Modelling Section

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,10 @@
 class Section < ActiveRecord::Base
+  default_scope order("position ASC")
+  belongs_to :post
+
+  validates :heading, :presence => true
+  validates :body, :presence => true
+  validates :position, :presence => true,
+                       :numericality => { :greater_than => 0}
 end
+
