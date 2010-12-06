c49a57a8fce6a923673b7683bfbd3c8742b3acd7
aRailsDemo <arailsdemo@gmail.com>
2010-12-06 14:29:40 -0800

    Modelling Snippet

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,10 @@
 class Snippet < ActiveRecord::Base
+  belongs_to :section
+
+  validates :caption, :presence => true
+  validates :content, :presence => true
+
+  def post_sequence
+    section.post.sequence if section.post
+  end
 end
