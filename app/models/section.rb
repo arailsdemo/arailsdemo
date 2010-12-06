e7be057d225d687576d983f7c6768bc0aea2ca36
aRailsDemo <arailsdemo@gmail.com>
2010-12-06 14:30:48 -0800

    Updating the Section Model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,9 @@
 class Section < ActiveRecord::Base
   default_scope order("position ASC")
   belongs_to :post
+  has_one :snippet, :dependent => :destroy
+  accepts_nested_attributes_for :snippet, :reject_if => lambda{ |a| a[:content].blank? && a[:caption].blank? }
+  
 
   validates :heading, :presence => true
   validates :body, :presence => true
