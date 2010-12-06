7f3a1af61cac7cd690e9e83edcf92f9dd8ba9ea4
aRailsDemo <arailsdemo@gmail.com>
2010-12-06 14:36:57 -0800

    Coderay Helper Methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def coderay(text, lang)
+      CodeRay.scan(text, lang).div(
+          :css => :class,
+          :tab_width => 2)
+  end
+  
+  def coderay_languages
+    Hash[*CODERAY_LANGUAGES.map {|l| [l.humanize, l] }.flatten]
+  end
 end
