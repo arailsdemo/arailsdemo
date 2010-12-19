d0ad1e1fc360e80139d2a0954d5506b0c379c7db
aRailsDemo <arailsdemo@gmail.com>
2010-12-19 15:09:24 -0800

    Clicking Through Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
   default_scope order("sequence DESC")
-  
+
   has_many :sections, :dependent => :destroy
   accepts_nested_attributes_for :sections, :reject_if => :all_blank
-  
-  
+
+
   validates :title, :presence => true
   validates :sequence, :presence => true,
                        :numericality => { :greater_than => 0 }
@@ -13,5 +13,10 @@ class Post < ActiveRecord::Base
   def build_section_and_snippet
     sections.build if sections.empty?
     sections.each{ |section| section.build_snippet if section.snippet.nil?}
+  end
+
+  def post(position)
+    increment = position == :previous ? -1 : 1
+    Post.find_by_sequence(self.sequence + increment)
   end
 end
