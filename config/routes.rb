86774096c59a0db8ce9af3eedad0027d5c35a638
aRailsDemo <arailsdemo@gmail.com>
2011-01-09 13:56:47 -0800

    Adding the Sessions Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Arailsdemo::Application.routes.draw do
+  post 'login', :to => 'sessions#create'
+  match 'login', :to => 'sessions#new', :as => 'login'
+  match 'logout', :to => 'sessions#destroy', :as => 'logout'
+
   resources :snippets
 
   resources :sections
