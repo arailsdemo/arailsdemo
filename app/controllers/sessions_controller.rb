e9c6c2279fff5ba322d7732ae03c99c8e9fe75d7
aRailsDemo <arailsdemo@gmail.com>
2011-01-09 13:54:17 -0800

    Building a Sessions Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    session[:pw] = params[:password]
+    if admin?
+      redirect_to root_url, :notice => "Successfully logged in"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "Logged out"
+    redirect_to root_url
+  end
+end
